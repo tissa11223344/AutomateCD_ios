ab96a3ce2cf2407436cdf666f8904c3c0e8ee7e5
nilu.walmik@tissatech.com <nilu.walmik@tissatech.com>
2024-04-01 15:01:01 +0530

    initial code

---
+++ AutomateCD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutomateCD
+//
+//  Created by Tissa Macbook on 01/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
